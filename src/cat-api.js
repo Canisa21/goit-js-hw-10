87b823032df9b9ca8c1f17cb169958debd6be35b
Canisa21 <jablonska.paul@gmail.com>
2024-02-27 21:49:58 +0100

    create cat-api

---
+++ src/cat-api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+export const fetchBreeds = () => {
+  axios.defaults.headers.common['x-api-key'] =
+    'live_VfZRjoVijaZAmOIh3ElYggu8fh1oObwL483D55lHPYfNjoa1T9EHzkKaS0i2eaMd';
+
+  return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+};
