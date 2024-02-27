9d6a4dde6c4b6a2ec5993d1d39bed4eeef091f07
Canisa21 <jablonska.paul@gmail.com>
2024-02-27 21:59:22 +0100

    styles.css

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-
 export const fetchBreeds = () => {
   axios.defaults.headers.common['x-api-key'] =
     'live_VfZRjoVijaZAmOIh3ElYggu8fh1oObwL483D55lHPYfNjoa1T9EHzkKaS0i2eaMd';
 
   return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(res => res.data);
 };
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+
+try {
+  loader.classList.remove('hidden');
+  fetchBreeds().then(data => renderSelect(data));
+} catch (error) {
+  console.log(error);
+}
